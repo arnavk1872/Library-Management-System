98033a7a4a50b32bdead37464daf0374fb4137e3
Arnav khajuria <arnavk1802@gmail.com>
2024-02-12 16:58:20 +0530

    Changes 3.0

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 
-
-function Borrow(event) {
-    const row = event.target.closest('tr');
-
-    const statusCell = row.querySelector('td:nth-child(4)');
-
-    statusCell.textContent = statusCell.textContent === 'Available' ? 'Borrowed' : 'Borrowed';
-
-    alert(`Book status changed to ${statusCell.textContent}`);
-
-    
-}   
-
 class Book {
     constructor(title, author) {
         this.title = title;
@@ -20,19 +7,18 @@ class Book {
     }
 
     toggleBorrowStatus() {
-        this.isBorrowed = !this.isBorrowed;
+        this.isBorrowed = true;
     }
 }
+
 class Display {
     book = new Book();
     add(book) {
         let uiString = `<tr>
-                          
                             <td>${book.title}</td>
                             <td>${book.author}</td>
                             <td>${book.isBorrowed ? 'Borrowed' : 'Available'}</td>
-                            <td><button> ${book.isBorrowed ? 'Return' : 'Borrow'} </button></td>
-                            
+                            <td class="borrow"><button onClick="Borrow(event)"> ${book.isBorrowed ? 'Return' : 'Borrow/Return'} </button></td> 
                         </tr>`;
         bookList.innerHTML += uiString;
     };
@@ -46,21 +32,16 @@ class Display {
         else {
             return true;
         }
-    }
-    toggleBorrowStatus(index) {
-        if (index >= 0 && index < this.bookList.length) {
-            const book = this.bookList[index];
-            book.toggleBorrowStatus();
-            this.updateBookList();
-            alert(`${book.title} ${book.isBorrowed ? 'borrowed' : 'returned'} successfully!`);
-        } else {
-            alert('Invalid book index.');
-        }
-    }
-    // saveToLocalStorage() {
-    //     localStorage.setItem('addBookList', JSON.stringify(this.bookList));
-    // }
+    }   
 }
+
+
+function Borrow(event) {
+        const row = event.target.closest('tr');
+        const statusCell = row.querySelector('td:nth-child(3)');
+        statusCell.textContent = statusCell.textContent === 'Available' ? 'Borrowed' : 'Available';
+        alert(`Book status changed to ${statusCell.textContent}`);
+    }
 
 
 let addBookForm = document.querySelector("#addBookForm");
